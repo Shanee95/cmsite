ee96967bfdcdfa3512e2576b6869c1621c4b178b
Shane <42964189+Shanee95@users.noreply.github.com>
2021-09-14 16:04:25 +0100

    Create votetest.php

---
+++ votetest.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Shane">
+    <meta name="generator" content="">
+    <title>Craftmine Network · Home</title>
+
+
+    <!-- Bootstrap core CSS -->
+<link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" rel="stylesheet">
+    <!-- Copy IP -->
+
+        <script src="JS/script.js"></script>
+        <script src="https://mcapi.us/scripts/minecraft.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.11.3/build/css/alertify.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.11.3/build/alertify.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/leonardosnt/mc-player-counter/dist/mc-player-counter.min.js"></script>
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="main.css" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark" style="background-image: url('IMG/bck.png')">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <nav class="nav nav-masthead justify-content-center float-md-start">
+      <a class="copyIP nav-link" data-clipboard-text="mc.craftmine.co.uk" aria-current="page" href="index.html">IP: mc.craftmine.co.uk</a>
+      </nav>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+        <a class="nav-link" href="#">Vote</a>
+        <a class="nav-link" href="https://discord.com/invite/GHdqTwD">Discord</a>
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+    <div class="logo_area">
+<center>
+<img src="IMG/Logo.png" alt="Logo">
+</center>
+</div>
+    <p class="lead">Craftmine is a friendly network for offline & online players. Currently offering <b>Survival</b> and <b>Skyblock</b>. To keep up with the latest news and updates, be sure to join our Discord!</p>
+
+  </main>
+
+  <footer class="mt-auto text-white-50">
+Craftmine 2019-2021
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
